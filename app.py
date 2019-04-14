e639e6b30e1f7069c449f5e54759b243f36b78a3
Simon <simonhesiea@gmail.com>
2019-04-14 19:00:36 +0200

    fix

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     html ="<html><head><title>Dashboard Test</title><meta charset='utf-8'>"
-    html+="<style> font-family:arial,sans;</style></head>"
+    html+="<style> html{font-family:arial,sans;}</style></head>"
     html+="<h1>Today is : {0}<h1/>".format(datetime.datetime.now())
     return html
 
