12ef7f76ab0a657332a5ad84ec71f21ab9eb1333
Simon <simonhesiea@gmail.com>
2019-04-14 18:59:19 +0200

    fix

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def index():
     html ="<html><head><title>Dashboard Test</title><meta charset='utf-8'>"
     html+="<style> font-family:arial,sans;</style></head>"
-    html+= "<h1>Today is : {0}<h1/>".format(datetime.datetime.now())
+    html+="<h1>Today is : {0}<h1/>".format(datetime.datetime.now())
     return html
 
 if __name__ == "__main__":
