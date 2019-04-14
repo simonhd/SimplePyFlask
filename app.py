fb25258c7889522ad5d7a49ef85f80c53cc02fb5
Simon <simonhesiea@gmail.com>
2019-04-14 18:50:14 +0200

    Datetime

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>Welcome<h1/>"
+    return "<h1>Today is : {0}<h1/>".format(datetime.datetime.now())
 
 if __name__ == "__main__":
     app.run()
