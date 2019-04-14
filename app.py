b5e095f5a8c4be560b97051e450df07af7caab68
Simon <simonhesiea@gmail.com>
2019-04-14 18:56:38 +0200

    html

--- app.py
+++ app.py
@@ -5,7 +5,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>Today is : {0}<h1/>".format(datetime.datetime.now())
+    html ="<html><head><title>Dashboard Test</title><meta charset='utf-8'>"
+    html+="<style> font-family:arial,sans;</style></head>"
+    html+= "<h1>Today is : {0}<h1/>".format(datetime.datetime.now())
+    return html
 
 if __name__ == "__main__":
     app.run()
